8def9558373dc1c7a7a1932cb518c496efba279d
Mattias <neptos89@gmail.com>
2019-02-03 12:20:13 +0200

    Added first version of custom url gatherer

---
+++ custom-url-gatherer/Gatherer.py
@@ -0,0 +1,31 @@
+import requests
+import os
+import time
+import datetime
+import GracefulInterruptHandler as gih
+
+sensor_endpoint = os.environ['SENSOR_ENDPOINT']
+ingester_endpoint = os.environ['INGESTER_ENDPOINT']
+data_points = []
+
+def AggregateDataPoints():
+    global data_points
+    total_sum = sum(data_points)
+    aggregate_average = total_sum/len(data_points)
+    data_points = []
+    return aggregate_average
+
+def SendAggregateDataToIngester():
+    average_temp = AggregateDataPoints()
+    requests.post(ingester_endpoint + "api/Temperature", json={'Value': average_temp, 'Timestamp': datetime.datetime.now().strftime('%Y-%m-%dT%H:%M:%SZ')})
+
+def AddDataPoint():
+    temperature = requests.get(sensor_endpoint).text
+    data_points.append(float(temperature))
+
+with gih.GracefulInterruptHandler() as hander:
+    while True:
+        for x in range(30): # 5minutes
+            AddDataPoint()
+            time.sleep(10)
+        SendAggregateDataToIngester()
