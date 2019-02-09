5b0b79959c85b976b988b7910cf6e459e071d95e
Mattias <neptos89@gmail.com>
2019-02-09 11:19:45 +0200

    Added IoC configuration of temperature publisher

---
+++ ingester/Ingester.IoC/ConfigureServices.cs
@@ -0,0 +1,14 @@
+﻿using Ingester.Application.Contracts;
+using Ingester.Infrastructure.Messages.Senders;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Ingester.IoC
+{
+    public static class ConfigureServices
+    {
+        public static void AddCustomServices(this IServiceCollection services)
+        {
+            services.AddTransient<ITemperaturePublisher, TemperaturePublisher>();
+        }
+    }
+}
