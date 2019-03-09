efc1ed3117fb47712705c94801e2f9bdab4058c7
Mattias <neptos89@gmail.com>
2019-03-09 19:13:52 +0200

    Updated fmi gatherer to gather more than just temperature data

--- fmi-gatherer/Gatherer.py
+++ fmi-gatherer/Gatherer.py
@@ -8,8 +8,8 @@ import GracefulInterruptHandler as gih
 ingester_endpoint = os.environ['INGESTER_ENDPOINT']
 last_timestamp = ''
 
-def SendDataToIngester(temperature, time):
-    result = requests.post(ingester_endpoint + "api/Temperature", json={'Value': temperature, 'Timestamp': time, 'SensorId': '0778f7bf-881f-4814-b939-ccd580aa5ddf', 'Location': 'Vasa'})
+def SendDataToIngester(value, time, measurementType):
+    result = requests.post(ingester_endpoint + "api/" + measurementType, json={'Value': value, 'Timestamp': time, 'SensorId': '0778f7bf-881f-4814-b939-ccd580aa5ddf', 'Location': 'Vaasa'})
 
 def AddDataPoint():
     global last_timestamp
@@ -17,14 +17,43 @@ def AddDataPoint():
     response = wfs.getfeature(storedQueryID='fmi::observations::weather::simple', storedQueryParams={'Place':'vaasa'})
     xml = response.read()
     root = ET.fromstring(xml)
-    for member in root[-11:]:
-        if member[0][2].text == 't2m':
-            temperature = member[0][3].text
-            time = member[0][1].text
+    for latestData in root[-11:]:
+        for member in latestData:
+            memberKey = member[2].text
+            memberValue = member[3].text
+            time = member[1].text
+            if memberKey == 't2m':
+                temperature = memberValue
+            elif memberKey == 'ws_10min':
+                windSpeed = memberValue
+            elif memberKey == 'wg_10min':
+                windGust = memberValue
+            elif memberKey == 'wd_10min':
+                windDirection = memberValue
+            elif memberKey == 'rh':
+                relativeHumidity = memberValue
+            elif memberKey == 'td':
+                dewPoint = memberValue
+            elif memberKey == 'r_1h':
+                precipitationAmount = memberValue
+            elif memberKey == 'snow_aws':
+                snowDepth = memberValue
+            elif memberKey == 'p_sea':
+                pressure = memberValue
+            elif memberKey == 'vis':
+                visibility = memberValue
     
     if time != last_timestamp:
         last_timestamp = time
-        SendDataToIngester(temperature, time)
+        SendDataToIngester(temperature, time, "Temperature")
+        SendDataToIngester(windSpeed, time, "WindSpeed")
+        SendDataToIngester(windGust, time, "WindGust")
+        SendDataToIngester(windDirection, time, "WindDirection")
+        SendDataToIngester(relativeHumidity, time, "RelativeHumidity")
+        SendDataToIngester(dewPoint, time, "DewPoint")
+        SendDataToIngester(precipitationAmount, time, "Precipitation")
+        SendDataToIngester(pressure, time, "Pressure")
+        SendDataToIngester(visibility, time, "Visibility")
 
 with gih.GracefulInterruptHandler() as hander:
     while True:
